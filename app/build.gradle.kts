8084d08743eecafb8865594f74960fd626aab31d
Ivan Paunovic <ivanpaunovic998@gmail.com>
2026-08-06 16:58:11 +0200

    Move lifecycle ViewModel Compose to version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
